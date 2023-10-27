dc79dce92d417995f89c05b95e0d91403b60cb5b
202116302 <wls5258@naver.com>
2023-10-27 14:45:29 +0900

    포맷팅 수정

--- thingspeak_log.py
+++ thingspeak_log.py
@@ -24,13 +24,15 @@ def main():
     del last_data[3:6]
     del last_data[8:13]
 
+    print(last_data)
+
 
 
     while True:
         try:
             urllib.request.urlopen(
                 "https://api.thingspeak.com/update?api_key=Y8YE9ZO5XFAMIFNJ&field1={:0.1f}&field2={:0.1f}&field3={:0.1f}&field4={:0.1f}&field5={:0.1f}&field6={:0.1f}&field7={:0.1f}&field8={:0.1f}".format(
-                    float(last_data[1]), float(last_data[2]), float(last_data[2]), float(last_data[3]), float(last_data[4]), float(last_data[5]), float(last_data[6]), float(last_data[7])))
+                    float(last_data[1]), float(last_data[2]), float(last_data[3]), float(last_data[4]), float(last_data[5]), float(last_data[6]), float(last_data[7]), float(last_data[8])))
 
         except RuntimeError as error:
             print(error.args[0])
@@ -52,6 +54,6 @@ def main():
         except KeyboardInterrupt:
             print("Terminated by Keyboard")
             break
-
+#
 if __name__ == '__main__':
     main()
